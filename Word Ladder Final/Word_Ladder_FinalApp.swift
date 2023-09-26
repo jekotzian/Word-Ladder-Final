d79379bffa3f10e3c741a6df82debf36e21101cf
Jacob Kotzian <kotzian49@gmail.com>
2023-09-26 19:12:56 -0400

    Initial Commit

---
+++ Word Ladder Final/Word_Ladder_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_Ladder_FinalApp.swift
+//  Word Ladder Final
+//
+//  Created by Jake Kotzian on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_Ladder_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
